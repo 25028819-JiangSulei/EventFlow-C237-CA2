620c81203c1e34a97466f36b820625f926608af5
Sulei <25028819@myrp.edu.sg>
2026-07-20 15:39:43 +0800

    Fix port for Render deployment

--- app.js
+++ app.js
@@ -140,6 +140,7 @@ app.get('/', (req, res) => {
   res.redirect('/events');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
